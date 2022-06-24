6230acae3dcffafb73016dd4218947265adca452
cheerfulperson <yagor.baradavkin@gmail.com>
2022-06-24 22:03:28 +0300

    feat: add rectangle drawing

--- src/modules/draw-actions.ts
+++ src/modules/draw-actions.ts
@@ -10,7 +10,7 @@ class DrawActions {
     const { x: x0, y: y0 } = robot.getMousePos();
     const radius = Number(params[1]);
     let angle = 0;
-    const draw = function drawStep() {
+    const draw = () => {
       if (angle > 360) {
         this.isDrawing = false;
         return;
@@ -21,19 +21,47 @@ class DrawActions {
       angle += 1;
       robot.mouseClick();
       robot.dragMouse(x, y);
-      drawStep();
+      draw();
     };
     draw();
   }
 
   public drawRectangle(params: string[]): void {
+    console.log(this.isDrawing);
     if (this.isDrawing) return;
     this.isDrawing = true;
+
+    const { x: x0, y: y0 } = robot.getMousePos();
+    const a = Number(params[1]);
+    const b = Number(params[2]);
+    let dx = 0;
+    let dy = 0;
+
+    const draw = () => {
+      if (dx < a && dy === 0) {
+        dx += 1;
+      } else if (dy < b && dx === a) {
+        dy += 1;
+      } else if (dx > 0 && dy === b) {
+        dx -= 1;
+      } else {
+        dy -= 1;
+      }
+
+      robot.mouseClick();
+      robot.moveMouseSmooth(x0 + dx, y0 + dy, 1);
+      if (dx === 0 && dy === 0) {
+        this.isDrawing = false;
+        return;
+      }
+      draw();
+    };
+    draw();
   }
 
   public drawSquare(params: string[]): void {
-    if (this.isDrawing) return;
-    this.isDrawing = true;
+    params.push(params[1]);
+    this.drawRectangle(params);
   }
 }
 
