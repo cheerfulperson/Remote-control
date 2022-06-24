f9eadb48e6fe841a3e3d1adf1d31cdf34f5dafe4
cheerfulperson <yagor.baradavkin@gmail.com>
2022-06-24 22:05:18 +0300

    feat: implement screen actions module

---
+++ src/modules/screen-actions.ts
@@ -0,0 +1,11 @@
+import robot from 'robotjs';
+import Jimp from 'jimp/*';
+import { WebSocket } from 'ws';
+
+class ScreenActions {
+  public sendScreen(socket: WebSocket): void {
+
+  }
+}
+
+export default ScreenActions;
